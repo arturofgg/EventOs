3ae713d1347d1319cd031824ceb4b45e15322437
arturofgg <arturofdezgg17@gmail.com>
2024-01-11 14:06:46 +0100

    Arreglada disposición de login y register y paso de uno a otro

--- app/src/main/java/com/example/eventos/Register.java
+++ app/src/main/java/com/example/eventos/Register.java
@@ -1,12 +1,20 @@
 package com.example.eventos;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void openLogin(View v) {
+        Intent intent = new Intent(Register.this, Login.class);
+        startActivity(intent);
     }
 }
