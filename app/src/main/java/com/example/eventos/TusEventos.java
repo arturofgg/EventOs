1f5520b562d2058f68b11087a05a9e95b2363437
luis <luise3840@gmail.com>
2024-01-14 19:19:22 +0100

    añadida la ventana de eventos

---
+++ app/src/main/java/com/example/eventos/TusEventos.java
@@ -0,0 +1,14 @@
+package com.example.eventos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TusEventos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tus_eventos);
+    }
+}
