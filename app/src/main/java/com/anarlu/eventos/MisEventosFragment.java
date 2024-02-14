5b7421be5373dcafa779ff21b043575a581a7958
arturofgg <arturofdezgg17@gmail.com>
2024-02-14 14:04:52 +0100

    borrado y toolbars unicas

--- app/src/main/java/com/anarlu/eventos/MisEventosFragment.java
+++ app/src/main/java/com/anarlu/eventos/MisEventosFragment.java
@@ -69,13 +69,10 @@ public class MisEventosFragment extends Fragment {
     private EventoAdapter adapter;
     private List<Evento> eventos;
 
-    private Button logout,borrar;
-
     private FirebaseAuth mAuth;
     private FirebaseFirestore mFirestore;
     private ImageView user2,creacion;
     private Toolbar toolbar;
-    private static final int REQUEST_CAMERA_PERMISSION=2020;
 
     private UCrop.Options options;
 
@@ -103,9 +100,7 @@ public class MisEventosFragment extends Fragment {
 
         mFirestore=FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
-        logout = view.findViewById(R.id.logout);
         user2 = view.findViewById(R.id.imageView2);
-        borrar=view.findViewById(R.id.delete);
         options = new UCrop.Options();
         creacion=view.findViewById(R.id.imageView5);
 
@@ -133,31 +128,6 @@ public class MisEventosFragment extends Fragment {
         } else {
             Toast.makeText(getActivity(), "Imagen no dispobible", Toast.LENGTH_SHORT).show();
         }
-
-        user2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(pickPhoto, REQUEST_CAMERA_PERMISSION);
-            }
-        });
-
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                logout();
-
-            }
-        });
-
-        borrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                borrarUsuario();
-            }
-        });
 
         creacion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -191,279 +161,16 @@ public class MisEventosFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Configuración de la Toolbar
-        toolbar.setTitle("Mis Eventos");
+        // Configurar la Toolbar
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        AppCompatActivity activity = (AppCompatActivity) requireActivity();
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setTitle("Mis Eventos");
 
         creacion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CrearNuevoEvento();
-            }
-        });
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser Fuser = mAuth.getCurrentUser();
-        if (Fuser == null) {
-            irLogin();
-        } else {
-            if(Fuser.getPhotoUrl()!=null){
-                loadFirebaseImage(Fuser.getPhotoUrl());
-            }else {
-                Uri imagenUri=obtenerNuevaImagenSeleccionada();
-                if(imagenUri!=null){
-                    cargarNuevaImagen(imagenUri);
-                }else {
-                    if(Fuser.getPhotoUrl()!=null){
-                        loadFirebaseImage(Fuser.getPhotoUrl());
-                    }
-                }
-            }
-
-            // Aquí es donde añades el código para descargar la imagen de Firebase Storage
-            StorageReference storageRef = FirebaseStorage.getInstance().getReference();
-            StorageReference pathReference = storageRef.child("perfil/"+Fuser.getUid());
-
-            File localFile = null;
-            try {
-                localFile = File.createTempFile("images", "jpg");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            File finalLocalFile = localFile;
-            pathReference.getFile(localFile)
-                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                            // El archivo se ha descargado con éxito
-                            Uri downloadedImageUri = Uri.fromFile(finalLocalFile);
-                            cargarNuevaImagen(downloadedImageUri);
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception exception) {
-                            // El archivo no se ha descargado correctamente
-                        }
-                    });
-        }
-    }
-
-    // Agregar los métodos onCreateOptionsMenu y onOptionsItemSelected para manejar el menú de la Toolbar
-    @Override
-    public void onCreateOptionsMenu(@NonNull android.view.Menu menu, @NonNull android.view.MenuInflater inflater) {
-        inflater.inflate(R.menu.menu_toolbar, menu);
-        super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.action_settings) {
-            // Acción para el elemento de menú de ajustes
-            Toast.makeText(requireActivity(), "Settings clicked", Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (item.getItemId() == R.id.action_profile) {
-            // Acción para el elemento de menú de perfil
-            Toast.makeText(requireActivity(), "Profile clicked", Toast.LENGTH_SHORT).show();
-            return true;
-        } else {
-            return super.onOptionsItemSelected(item);
-        }
-    }
-
-
-    private void cargarNuevaImagen(Uri imagenUri) {
-        Glide.with(getActivity())
-                .load(imagenUri)
-                .circleCrop()
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .skipMemoryCache(true)
-                .into(user2);
-        Glide.get(getActivity().getApplicationContext()).clearMemory();
-    }
-
-
-    private Uri obtenerNuevaImagenSeleccionada() {
-        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-        String imagenUriStr = sharedPref.getString("imagenUri", null);
-        if (imagenUriStr != null) {
-            return Uri.parse(imagenUriStr);
-        }
-        return null;
-    }
-
-    private void logout() {
-        mAuth.signOut();
-        mGoogleSignInClient.signOut();
-        Intent i = getActivity().getBaseContext().getPackageManager().getLaunchIntentForPackage(getActivity().getBaseContext().getPackageName());
-        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(i);
-        irLogin();
-    }
-
-    private void irLogin(){
-        Intent intent=new Intent(getActivity(), LRFragmentsActivity.class);
-        startActivity(intent);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        options.setCircleDimmedLayer(true);
-
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == REQUEST_CAMERA_PERMISSION) {
-                Uri imageUri = data.getData();
-                if (imageUri != null) {
-                    user2.setImageURI(imageUri);
-
-                    Uri destinoUri = Uri.fromFile(new File(getActivity().getCacheDir(), "imagenRecortada"));
-                    UCrop.of(imageUri, destinoUri).withOptions(options).start(getActivity());
-                } else {
-                    // Puede que haya casos donde la imagenUri sea nula, debes manejarlo adecuadamente
-                    Toast.makeText(getActivity(), "Error al obtener la imagen de la cámara", Toast.LENGTH_SHORT).show();
-                }
-            } else if (requestCode == UCrop.REQUEST_CROP) {
-                handleCropResult(data);
-            }
-        } else if (resultCode == UCrop.RESULT_ERROR) {
-            // Manejar el error del recorte
-            final Throwable cropError = UCrop.getError(data);
-            if (cropError != null) {
-                Log.e("UCrop", "Error al recortar la imagen", cropError);
-                Toast.makeText(getActivity(), "Error al recortar la imagen", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void handleCropResult(Intent data) {
-        final Uri resultUri = UCrop.getOutput(data);
-
-        if (resultUri != null) {
-            Glide.with(getActivity())
-                    .load(resultUri)
-                    .circleCrop()
-                    .diskCacheStrategy(DiskCacheStrategy.NONE)
-                    .skipMemoryCache(true)
-                    .into(user2);
-
-            cambiarFotoPerfil(resultUri);
-        } else {
-            Toast.makeText(getActivity(), "Error al obtener la imagen recortada", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-
-    private void subirFoto(Uri resultUri) {
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        String userId = mAuth.getCurrentUser().getUid();
-        StorageReference storageRef = storage.getReference();
-        StorageReference fotoRef = storageRef.child("perfil/" + userId);
-
-        // Eliminar la foto anterior
-        fotoRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void unused) {
-                Log.d("Eliminar foto", "Eliminacion exitosa");
-
-                // Subir la nueva foto después de eliminar la anterior
-                UploadTask uploadTask = fotoRef.putFile(resultUri);
-
-                uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        fotoRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                            @Override
-                            public void onSuccess(Uri uri) {
-                                String fotoUrl = uri.toString();
-                                Map<String, Object> datosUsuario = new HashMap<>();
-                                datosUsuario.put("foto", fotoUrl);
-
-                                FirebaseUser currentUser = mAuth.getCurrentUser();
-                                if (currentUser != null && mFirestore!=null) {
-                                    mFirestore.collection("usuarios").document(currentUser.getUid()).update(datosUsuario).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                        @Override
-                                        public void onSuccess(Void unused) {
-                                            Toast.makeText(getActivity(), "Foto guardada correctamente", Toast.LENGTH_SHORT).show();
-                                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                                    .setPhotoUri(Uri.parse(fotoUrl)) // Aquí debes poner la URL de la nueva foto
-                                                    .build();
-
-                                            currentUser.updateProfile(profileUpdates)
-                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                        @Override
-                                                        public void onComplete(@NonNull Task<Void> task) {
-                                                            if (task.isSuccessful()) {
-                                                                Log.d(TAG, "User profile updated.");
-                                                            }
-                                                        }
-                                                    });
-                                        }
-                                    }).addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Toast.makeText(getActivity(), "Error al actualizar la foto", Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
-                                }
-                            }
-                        });
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getActivity(), "Error al subir la nueva foto", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.e("Eliminar foto", "Error al eliminar foto:" + e.getMessage());
-                Toast.makeText(getActivity(), "Error al eliminar foto anterior: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-
-    private void cambiarFotoPerfil(Uri nuevaImagenUri) {
-        subirFoto(nuevaImagenUri);
-        FirebaseUser currentUser=mAuth.getCurrentUser();
-        if(currentUser!=null){
-            guardarImagen(nuevaImagenUri,currentUser.getUid());
-        }
-    }
-
-    private void guardarImagen(Uri imagenUri,String userId) {
-        FirebaseUser currentUser=mAuth.getCurrentUser();
-        if(currentUser!=null && currentUser.getUid().equals(userId)) {
-            SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPref.edit();
-            editor.putString("imagenUri", imagenUri.toString());
-            editor.apply();
-        }
-    }
-
-    private void loadFirebaseImage(Uri photoUrl){
-        Glide.with(getActivity()).load(photoUrl).circleCrop().diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).into(user2);
-    }
-
-    private void borrarUsuario(){
-        FirebaseUser user=mAuth.getCurrentUser();
-        user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(getActivity(), "Usuario eliminado correctamente", Toast.LENGTH_SHORT).show();
-                    irLogin();
-                }
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getActivity(), "Error al eliminar usaurio", Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/anarlu/eventos/PaginaPrincipal.java
+++ app/src/main/java/com/anarlu/eventos/PaginaPrincipal.java
@@ -14,22 +14,18 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class PaginaPrincipal extends AppCompatActivity { // Cambio aquí a BaseActivity
 
     private BottomNavigationView bottomNavigationView;
-    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_principal);
 
-        // Configurar la Toolbar
-        toolbar =(Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment selectedFragment = null;
+                String title = "";
                 int itemId = item.getItemId();
                 if (itemId == R.id.nav_option1) {
                     selectedFragment = new MisEventosFragment();
@@ -38,38 +34,12 @@ public class PaginaPrincipal extends AppCompatActivity { // Cambio aquí a BaseA
                 } else if (itemId == R.id.nav_option3) {
                     selectedFragment = new EventosFragment();
                 }
-
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_pg, selectedFragment).commit();
                 return true;
             }
         });
 
         // Cargar el primer fragmento por defecto
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_pg, new EventosFragment()).commit();
-        // Configurar la Toolbar inicialmente con el primer fragmento
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
-        // Configurar la Toolbar inicialmente con el primer fragmento
-        return true;
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // Configurar la Toolbar después de inflar el menú
-        updateToolbar("Eventos", true, true);
-        return super.onPrepareOptionsMenu(menu);
-    }
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        // Manejar los clics en los elementos del menú de la Toolbar aquí si es necesario
-        return super.onOptionsItemSelected(item);
-    }
-    // Método para actualizar dinámicamente la Toolbar
-    private void updateToolbar(String title, boolean showSettingsIcon, boolean showProfileIcon) {
-        getSupportActionBar().setTitle(title);
-        toolbar.getMenu().findItem(R.id.action_settings).setVisible(showSettingsIcon);
-        toolbar.getMenu().findItem(R.id.action_profile).setVisible(showProfileIcon);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_pg, new MisEventosFragment()).commit();
     }
 }
