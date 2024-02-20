86bfc4c2c342fff1c05c90eb98c9cf5796742d30
Cumcerberus <megagodxxx@gmail.com>
2024-02-20 10:55:41 +0100

    eventos añadidos a eventos

--- app/src/main/java/com/anarlu/eventos/EventosFragment.java
+++ app/src/main/java/com/anarlu/eventos/EventosFragment.java
@@ -1,64 +1,68 @@
 package com.anarlu.eventos;
 
+import static android.content.ContentValues.TAG;
+
+import android.app.Instrumentation;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EventosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventosFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView recyclerView;
+    private EventoAdapter adapter;
+    private List<Evento> eventos;
+    private FirebaseFirestore mFirestore;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public EventosFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EventosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EventosFragment newInstance(String param1, String param2) {
-        EventosFragment fragment = new EventosFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_eventos,container,false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_eventos, container, false);
+        recyclerView=view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        mFirestore=FirebaseFirestore.getInstance();
+
+        eventos=new ArrayList<>();
+        adapter=new EventoAdapter(eventos);
+        recyclerView.setAdapter(adapter);
+
+            mFirestore.collection("Eventos").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        eventos.clear();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Evento evento = document.toObject(Evento.class);
+                            eventos.add(evento);
+                        }
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        Log.w(TAG, "Error getting documents.", task.getException());
+                    }
+                }
+            });
+
+
+
+        return view;
     }
 }
