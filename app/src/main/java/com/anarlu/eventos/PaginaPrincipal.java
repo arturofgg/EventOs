9303da2f5a94c9596d6cec8ad79d2f43be135ce5
arturofgg <arturofdezgg17@gmail.com>
2024-02-20 10:47:09 +0100

    Deslizamiento añadido entre fragmentos

--- app/src/main/java/com/anarlu/eventos/PaginaPrincipal.java
+++ app/src/main/java/com/anarlu/eventos/PaginaPrincipal.java
@@ -1,16 +1,17 @@
 package com.anarlu.eventos;
 
+import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.view.MenuItem;
-
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class PaginaPrincipal extends AppCompatActivity { // Cambio aquí a BaseActivity
 
+    private ViewPager viewPager;
     private BottomNavigationView bottomNavigationView;
 
     @Override
@@ -18,26 +19,74 @@ public class PaginaPrincipal extends AppCompatActivity { // Cambio aquí a BaseA
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_principal);
 
+        viewPager = findViewById(R.id.viewPagerPaginaPrincipal);
+        // Configurar el adaptador de fragmentos para el ViewPager
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return new MisEventosFragment();
+                    case 1:
+                        return new ChatFragment();
+                    case 2:
+                        return new EventosFragment();
+                    default:
+                        return null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 3;
+            }
+        });
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selectedFragment = null;
-                String title = "";
                 int itemId = item.getItemId();
                 if (itemId == R.id.nav_option1) {
-                    selectedFragment = new MisEventosFragment();
+                    viewPager.setCurrentItem(0);
+                    return true;
                 } else if (itemId == R.id.nav_option2) {
-                    selectedFragment = new ChatFragment();
+                    viewPager.setCurrentItem(1);
+                    return true;
                 } else if (itemId == R.id.nav_option3) {
-                    selectedFragment = new EventosFragment();
+                    viewPager.setCurrentItem(2);
+                    return true;
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_pg, selectedFragment).commit();
-                return true;
+                return false;
             }
         });
 
+        // Configurar el ViewPager para sincronizarse con la selección del BottomNavigationView
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        bottomNavigationView.setSelectedItemId(R.id.nav_option1);
+                        break;
+                    case 1:
+                        bottomNavigationView.setSelectedItemId(R.id.nav_option2);
+                        break;
+                    case 2:
+                        bottomNavigationView.setSelectedItemId(R.id.nav_option3);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
+
         // Cargar el primer fragmento por defecto
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_pg, new MisEventosFragment()).commit();
+        viewPager.setCurrentItem(0);
     }
 }
